355e0cb5b95b2bf0c34c0be0e6342d2e95d7df6c
Yam Kisluk <yamk707@gmail.com>
2026-05-09 21:51:28 +0300

    Implemented the type builtin for shell. Type - write a description of command type.

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,23 @@ using namespace std;
 
 void echo(string str)
 {
-  //std::string out = str;
   str.erase(0,5);
   cout << str << "\n";
+}
+void type(string str)
+{
+  str.erase(0,5);
+  string builtin [] = {"echo","exit","type"};
+  //int size = sizeof(builtin) / sizeof(builtin[0]);
+  for(string command : builtin)
+  {
+    if(command == str)
+    {
+      cout << command << " is a shell builtin" << "\n";
+      return;
+    }
+  }
+  cout << str << ": not found" << "\n";
 }
 
 int main() {
@@ -25,6 +39,11 @@ int main() {
       echo(command);
       continue;
     }
+    if(command.substr(0,5)== "type ")
+    {
+      type(command);
+      continue;
+    }
     cout << command << ": command not found" << "\n";
   }
   return 0;
@@ -33,5 +52,6 @@ int main() {
 
 /*
 Commit: 
-New Loop for the REPL , add echo command and complety replaced cin with getline do to the stream object tokens are based on space and not a newline. 
+1. New Loop for the REPL , add echo command and complety replaced cin with getline do to the stream object tokens are based on space and not a newline. 
+2. Implemented the type builtin for shell. Type - write a description of command type.
 */
