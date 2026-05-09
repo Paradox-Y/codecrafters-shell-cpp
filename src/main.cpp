a6d9d56ff98023dad0628ef469aee32c2d9c8eeb
Yam Kisluk <yamk707@gmail.com>
2026-05-09 21:15:56 +0300

    New Loop for the REPL , add echo command and complety replaced cin with getline function to have the stream object tokens based on newlines and not spaces or tabs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 #include <string>
 
-using std::cout;
-using std::cin;
+using namespace std;
+//using std::cout;
+//using std::cin;
+
+
+void echo(string str)
+{
+  //std::string out = str;
+  str.erase(0,5);
+  cout << str << "\n";
+}
 
 int main() {
-  // Flush after every std::cout / std:cerr  UNDERSTAND THIS
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
-
-
-  std::string command="";
-  while(true)
+  // Flush after every std::cout / std:cerr - flush means the buffer (like a waiter) is seding the string (wiater delevers the food from the kichen) 
+  cout << unitbuf;
+  cerr << unitbuf;
+  string command="";
+  while(cout << "$ " && getline(cin,command) && command != "exit") 
   {
-    cout << "$ ";
-    cin >> command;
-    if(command == "exit")
-      return 0;
+    if(command.substr(0,5) == "echo ")
+    {
+      echo(command);
+      continue;
+    }
     cout << command << ": command not found" << "\n";
   }
-
+  return 0;
 }
+
+
+/*
+Commit: 
+New Loop for the REPL , add echo command and complety replaced cin with getline do to the stream object tokens are based on space and not a newline. 
+*/
