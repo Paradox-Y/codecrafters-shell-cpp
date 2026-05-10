87cc3ee40afb7e64077daf5f5d08f641106f85f4
Yam Kisluk <yamk707@gmail.com>
2026-05-10 23:39:01 +0300

    Extended the type builtin command to search for executable files using PATH.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string>
+#include <filesystem> 
+#include <unistd.h>//is the name of the header file that provides access to the POSIX operating system 
+
+namespace fs = std::filesystem;
 
 using namespace std;
-//using std::cout;
-//using std::cin;
 
+void scan_directory(const fs::path& p,const string& str);
 
 void echo(string str)
 {
   str.erase(0,5);
   cout << str << "\n";
 }
-void type(string str)
+void type(string str) //Works fine.
 {
   str.erase(0,5);
   string builtin [] = {"echo","exit","type"};
-  //int size = sizeof(builtin) / sizeof(builtin[0]);
   for(string command : builtin)
   {
     if(command == str)
@@ -26,6 +28,58 @@ void type(string str)
   }
   cout << str << ": not found" << "\n";
 }
+
+
+void typeExtanded (string command) // Working progress
+{
+  command.erase(0,5);
+  string builtin [] = {"echo","exit","type"};
+  for(string builtincommand : builtin)
+  {
+    if(builtincommand == command)
+    {
+      cout << command << " is a shell builtin" << "\n";
+      return;
+    }
+  }
+  fs::path p = fs::current_path(); //static function thats why we can imedatly call it. No need to constract a specific object instance.
+  scan_directory(p,command); // Now we check for files with the same name as the command.
+
+}
+
+bool executepermissions(const fs::path& p)
+{
+  return ::access(p.c_str(),X_OK) == 0;
+  //C-string = old C style string: array/pointer of char ending with '\0' (null terminator).
+  //X_OK is a flag meaning “check execute permission”.
+}
+ 
+void scan_directory(const fs::path& p,const string& command)
+{
+  if(is_directory(p))
+  {
+      for(const fs::directory_entry& x: fs::directory_iterator(p)){
+        const fs::path& f = x;
+        if(f.filename() == command && executepermissions(f))
+        {
+          cout << command << " is " << f.string() << '\n';
+          return;
+        }
+           
+        else{
+          //string n = f.extension().string();
+          //if(n== ".cpp" || n==".C" || ".cxx")
+          //cout << f.stem() << "is a C++ source file TESTTESTTEST\n";
+          //SkipL the file exists but lacks execute permissions, skip it and continue to the next directory.
+        }
+      }
+      cout << command <<": not found\n";
+
+  }
+}
+
+
+
 
 int main() {
   // Flush after every std::cout / std:cerr - flush means the buffer (like a waiter) is seding the string (wiater delevers the food from the kichen) 
@@ -41,7 +95,7 @@ int main() {
     }
     if(command.substr(0,5)== "type ")
     {
-      type(command);
+      typeExtanded(command);
       continue;
     }
     cout << command << ": command not found" << "\n";
@@ -54,4 +108,10 @@ int main() {
 Commit: 
 1. New Loop for the REPL , add echo command and complety replaced cin with getline do to the stream object tokens are based on space and not a newline. 
 2. Implemented the type builtin for shell. Type - write a description of command type.
+3. Extended the type builtin command to search for executable files using PATH.
+*/
+
+/*
+The PATH environment variable specifies a list of directories where the shell should look for executable programs.
+For example, if the PATH is set to /dir1:/dir2:/dir3, the shell would search for executables in /dir1, then /dir2, and finally /dir3, in that order.
 */
