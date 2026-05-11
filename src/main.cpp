6c1ce727941396cd7bc2bb273c3c6bb07dc80a9e
Yam Kisluk <yamk707@gmail.com>
2026-05-11 23:52:55 +0300

    New varible names

--- src/main.cpp
+++ src/main.cpp
@@ -10,14 +10,20 @@ void echo(std::string str);
 
 void type(std::string line) //Old version
 {
-  std::string command;
-  std::stringstream ss(line); 
-  ss >> command;
-  
+  std::stringstream ss(line);
 
+  std::string type_cmd;
+  ss >> type_cmd;
+
+  std::string command;
+  ss >> command;
+
+  if(command.empty())
+    return;
+  
   const std::string builtin_commands[] = {"echo","exit","type"}; 
 
-  for(std::string builtin : builtin_commands)
+  for(const std::string& builtin : builtin_commands)
   {
     if(builtin == command)
     {
@@ -30,6 +36,7 @@ void type(std::string line) //Old version
 
   const char* path_raw = getenv("PATH");
   const std::string path_env = path_raw ? path_raw : "";
+
   if (!path_env.empty()) {
     std::stringstream ss_path(path_env);
     std::string path;
